b276d35ab3bfdc90f79b7168b0fb82851a56cad7
Overlander <anuragnandal@hotmail.com>
2022-08-15 12:16:24 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_115323) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_004046) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  add_foreign_key "posts", "users"
 end
