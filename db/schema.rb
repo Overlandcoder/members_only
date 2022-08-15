45c187749205c1a28e04cd108c598041e00d3c2f
Overlander <anuragnandal@hotmail.com>
2022-08-15 08:59:37 -0400

    Drop users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_114404) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_115323) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,14 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_114404) do
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "posts", "users"
